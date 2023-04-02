417f42a2dfd99af3f6f565cd47d7022a79e9f6b9
rajeh-dalbah <rajeh.dalbah@gmail.com>
2023-04-02 15:41:22 +0000

    i updated javascript file and did a new lab

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var firstName = prompt("Enter your name");
+var gender = prompt("Enter your gender please...")
+if (gender == "male" || gender == "female"){
+    
+    } 
+else{
+    prompt("Please your gender...");
+    }
+        
+var age = prompt("Please enter your age...");
+if (age > 0){
+}
+else {
+    alert("Your age is equal to zero");
+}
+
+if (confirm("Are you want wants to skip the welcoming message?")) {
+  } 
+else if(gender =="male"){
+    alert("Welcome Mr" + " " + firstName);
+    
+    } 
+else if ( gender == "female"){
+    alert("Welcome Ms " + " " + firstName);
+    
+    }
+else {
+    alert("welcome" + " " +firstName);
+    
+  }
