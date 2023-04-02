20df533bba0626578083bf97b413241cd833d165
rajeh-dalbah <rajeh.dalbah@gmail.com>
2023-04-02 17:13:37 +0000

    fix message spelling

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,19 @@
 'use strict';
 
 var firstName = prompt("Enter your name");
-var gender = prompt("Enter your gender please...")
-if (gender == "male" || gender == "female"){
-    
-    } 
-else{
+var gender = prompt("Enter your gender please...").toLowerCase
+if (gender != "male" || gender != "female"){
     prompt("Please your gender...");
-    }
+    } 
         
 var age = prompt("Please enter your age...");
 if (age > 0){
 }
 else {
-    alert("Your age is equal to zero");
+    alert("Your age is less than or equal to zero");
 }
 
-if (confirm("Are you want wants to skip the welcoming message?")) {
+if (confirm("Are you wants to skip the welcoming message?")) {
   } 
 else if(gender =="male"){
     alert("Welcome Mr" + " " + firstName);
