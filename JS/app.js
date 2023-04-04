4e5bf611c88e705ffef4641c034fe3ffa8afa53d
rajeh-dalbah <rajeh.dalbah@gmail.com>
2023-04-04 19:24:50 +0000

    ask questions using functions

--- JS/app.js
+++ JS/app.js
@@ -29,3 +29,34 @@ else {
     alert("welcome" + " " +firstName);
     
   }
+  
+ let answers =[];
+ 
+  function creatQuestion(stringQuestion){
+    let question = prompt(stringQuestion).toLowerCase();
+    validateQuestion(question);
+}
+
+function validateQuestion (answer){
+    if (answer != ""){
+        answers.push(answer);
+     } 
+    else {
+         answers.push("Invaled");
+     }
+}
+
+
+    creatQuestion("It's your first time in our website?");
+    
+    creatQuestion("Do you like the design?");
+    
+    creatQuestion("are you will recummended our website to your friend?");
+    
+    
+function printAnswers(arr){
+    for (let i = 0;i<answers.length;i++)
+        console.log("answer of question " + i + " is : " +answers[i]);
+    }
+    
+ printAnswers(answers); 
